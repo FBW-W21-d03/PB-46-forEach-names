dd37ed9249bb0f3086988963ac8196d7b643f818
Enza <86748625+Enza-von-metaTronics@users.noreply.github.com>
2022-01-10 13:22:46 +0100

    PB46

---
+++ index.js
@@ -0,0 +1,12 @@
+//Aufgabenstellung - Teil 1
+const names = ["Sammy", "Oscar", "Oliver", "Ava", "Elijah", "Charlotte", "William", "Sophia", "James", "Amelia"];
+console.log(names);
+//Aufgabenstellung - Teil 2
+names.sort();
+console.log(names.sort());
+//Aufgabenstellung - Teil 3
+names.forEach(
+    (name) => {
+         console.log(name.length);
+    }
+);
