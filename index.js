17b9f0b3c972d596a2d611ba2471d1fc07f188e9
aisha2000-hub <aisha.noori2000@gmail.com>
2022-01-10 13:37:49 +0100

    Dir lösung von Aisha

---
+++ index.js
@@ -0,0 +1,14 @@
+// AUFGABE TEIL-1
+const names = ["Sammy", "Oscar", "Oliver", "Ava", "Elijah", "Charlotte", "William"," Sophia", "James", "Amelia"];
+
+//  AUFGABE TEIL - 2
+names.sort();
+console.log(names);
+
+// AUFGABE TEIL - 3
+names.forEach(
+    (name) => {
+     console.log("the lenth of name:", `${name} ${name.length}`)
+    }
+);
+
