ac6da3d0300840c0ac6373da1bec20213e40a75f
sprotte <metatron1972@live.de>
2022-01-10 13:20:18 +0100

    sprotte

---
+++ index.js
@@ -0,0 +1,18 @@
+"use strict";
+
+const employees = [
+  "Sammy",
+  "Oscar",
+  "Oliver",
+  "Ava",
+  "Elias",
+  "Charlotte",
+  "William",
+  "Sophia",
+  "James",
+  "Amelia",
+];
+employees.sort();
+employees.forEach((employeeName) => {
+  console.log(employeeName);
+});
