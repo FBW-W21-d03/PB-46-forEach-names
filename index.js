7f674183eee08663346bd1ea6d0096ea193c0c5f
Mazen <mazen.taha88@gmail.com>
2022-01-10 13:55:02 +0100

    My Lösung

---
+++ index.js
@@ -0,0 +1,16 @@
+
+  
+
+const Namen = ["Sammy", "Oscar", "Oliver", "Ava", "Elijah", "Charlotte", "William"," Sophia", "James", "Amelia"];
+
+Namen.sort();
+console.log(Namen);
+
+
+Namen.forEach(
+    (name) => {
+     console.log(`${name} ${name.length}`)
+
+
+    }
+);
