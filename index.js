8c2217a619d6f93bdeb763229e771707bc2c93bd
kawanaza <kawanbabani@gmail.com>
2022-01-10 13:24:30 +0100

    Done

---
+++ index.js
@@ -0,0 +1,18 @@
+const myname = [
+  "Sammy",
+  "Oscar",
+  "Oliver",
+  "Ava",
+  "Elijah",
+  "Charlotte",
+  "William",
+  "Sophia",
+  "James",
+  "Amelia",
+];
+myname.sort();
+console.log(myname);
+myname.forEach((lengthName) => {
+  console.log(lengthName.length);
+  console.log(lengthName[(0, 0)]);
+});
