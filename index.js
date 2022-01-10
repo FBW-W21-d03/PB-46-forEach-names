dc8311d5ac84271998f4bb43673f0474c0344dec
Mohal1125 <rajwindersinghmohal@yahoo.com>
2022-01-10 13:23:50 +0100

    mein lösung

---
+++ index.js
@@ -0,0 +1,20 @@
+const namen = [
+  "Sammy",
+  "Oscar",
+  "Oliver",
+  "Ava",
+  "Elijah",
+  "Charlotte",
+  "William",
+  "Sophia",
+  "James",
+  "Amelia",
+];
+namen.sort();
+console.log(namen);
+
+namen.forEach(
+    (name) => {
+       console.log("Lenght of name" , name , name.length)
+    }
+    );
