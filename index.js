528003845417c60c84bafcffbc3293c61b05c741
Enza <86748625+Enza-von-metaTronics@users.noreply.github.com>
2022-01-10 14:24:18 +0100

    und noch..

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ console.log(names.sort());
 //Aufgabenstellung - Teil 3
 names.forEach(
     (name) => {
-         console.log("Die Länge der Buchstaben von " , name , "ist: " ,name.length , ".");
+         console.log("Die Länge der Buchstaben von" , name , "ist:" ,name.length );
     }
 );
