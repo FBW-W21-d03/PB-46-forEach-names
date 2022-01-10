b7a4f55198a3d5698691e6cf10892298d7d08a4b
jaber-1988 <jaber1988.1988@gmail.com>
2022-01-10 13:27:37 +0100

    jaber

---
+++ index.js
@@ -0,0 +1,9 @@
+const Namen=["Sammy", "Oscar", "Oliver", "Ava", "Elijah", "Charlotte","William", "Sophia", "James", "Amelia"];
+Namen.sort();
+console.log(Namen);
+
+Namen.forEach(
+    (name) => {
+         console.log(name.length);
+    }
+);
